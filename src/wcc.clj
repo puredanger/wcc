fdc3b7d0a4eb76ab0c0fc8cea0b12e77a7251e2e
puredanger <alexdmiller@yahoo.com>
2010-02-26 14:34:51 -0600

    Fix defaults in with-command-line and simplify count-words

--- src/wcc.clj
+++ src/wcc.clj
@@ -1,14 +1,13 @@
 (ns wcc
   	(:gen-class)
 	(:use [clojure.contrib command-line 
-			[duck-streams :only [read-lines writer]]
-			[str-utils2 :only [split]]]))
+			[duck-streams :only [read-lines writer]]]))
 
 (defn count-lines [file] 
 	(count (read-lines file)))
 
 (defn count-words [file]
-	(count (filter not-empty (split (slurp file) #"\s"))))
+	(count (seq (.split #"\s+" (slurp file)))))
 
 (defn count-chars [file] 
 	(count (slurp file)))
@@ -23,16 +22,18 @@
 	(with-command-line
 	  args 
 	  "Usage: wcc [-l|-w|-c] [-o out.txt] <file>..."
-	  [[lines? l? "Count lines" true]
-	   [words? w? "Count words" false]
-	   [chars? c? "Count chars" false]
+	  [[lines? l? "Count lines"]
+	   [words? w? "Count words"]
+	   [chars? c? "Count chars"]
 	   [out "The output file"]
 	   files]
-		
+
 		(binding [*out* (if out (writer out) *out*)]
-			(doall (map #(cond 
-					lines? (print-count % count-lines)
-					words? (print-count % count-words)
-					chars? (print-count % count-chars)
-					:else (print-invalid-option)) 
-				files)))))
+			(doall 
+				(map 
+					#(cond 
+						lines? (print-count % count-lines)
+						words? (print-count % count-words)
+						chars? (print-count % count-chars)
+						:else (print-invalid-option))
+					files)))))
