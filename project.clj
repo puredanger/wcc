687471f52cf4b32121f38aa2c1c8e23e06c12e5b
puredanger <alexdmiller@yahoo.com>
2010-02-25 22:05:54 -0600

    initial

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject wcc "0.0.1-SNAPSHOT"
+  :description "wc in Clojure, just for fun"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+		]
+  :main wcc
+)
---
+++ src/wcc.clj
@@ -0,0 +1,38 @@
+(ns wcc
+  	(:gen-class)
+	(:use [clojure.contrib command-line 
+			[duck-streams :only [read-lines with-out-writer writer]]
+			[str-utils2 :only [split]]]))
+
+(defn count-lines [file] 
+	(count (read-lines file)))
+
+(defn count-words [file]
+	(count (filter not-empty (split (slurp file) #"\s"))))
+
+(defn count-chars [file] 
+	(count (slurp file)))
+
+(defn print-count [file count-function]
+	(println (format "Counted %s: %s" file (count-function file))))
+	
+(defn print-invalid-option [] 
+	(println "Must specify one of -l, -w, or -c"))
+	
+(defn -main [& args]
+	(with-command-line
+	  args 
+	  "Usage: wc [-l|-w|-c] [-o out.txt] <file>..."
+	  [[lines? l? "Count lines" true]
+	   [words? w? "Count words" false]
+	   [chars? c? "Count chars" false]
+	   [out "The output file"]
+	   files]
+		
+		(binding [*out* (if out (writer out) *out*)]
+			(doall (map #(cond 
+					lines? (print-count % count-lines)
+					words? (print-count % count-words)
+					chars? (print-count % count-chars)
+					:else (print-invalid-option)) 
+				files)))))
